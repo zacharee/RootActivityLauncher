992e49da86e6f52f3e26dc0bd44f5b2addc55e88
zacharee <zachary.wander@gmail.com>
2020-04-04 13:49:30 -0400

    Remove unneeded coroutine call

--- app/src/main/java/tk/zwander/rootactivitylauncher/adapters/component/ActivityAdapter.kt
+++ app/src/main/java/tk/zwander/rootactivitylauncher/adapters/component/ActivityAdapter.kt
@@ -32,72 +32,70 @@ class ActivityAdapter(picasso: Picasso) : BaseComponentAdapter<ActivityAdapter, 
 
     inner class ActivityVH(view: View) : BaseComponentVH(view) {
         override fun bind(data: ActivityInfo) = launch {
-            withContext(Dispatchers.Main) {
-                itemView.apply {
-                    activity_name.text = data.label
-                    activity_cmp.text = data.info.name
+            itemView.apply {
+                activity_name.text = data.label
+                activity_cmp.text = data.info.name
 
-                    picasso.load(ActivityIconHandler.createUri(data.info.packageName, data.info.name))
-                        .fit()
-                        .centerInside()
-                        .into(activity_icon)
+                picasso.load(ActivityIconHandler.createUri(data.info.packageName, data.info.name))
+                    .fit()
+                    .centerInside()
+                    .into(activity_icon)
 
-                    set_extras.setOnClickListener {
+                set_extras.setOnClickListener {
+                    val d = items[adapterPosition]
+                    ExtrasDialog(context, constructComponentKey(d.info.packageName, d.info.name))
+                        .show()
+                }
+
+                enabled.setOnCheckedChangeListener(null)
+                enabled.isChecked = data.info.enabled
+                enabled.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
+                    override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
                         val d = items[adapterPosition]
-                        ExtrasDialog(context, constructComponentKey(d.info.packageName, d.info.name))
-                            .show()
-                    }
-
-                    enabled.setOnCheckedChangeListener(null)
-                    enabled.isChecked = data.info.enabled
-                    enabled.setOnCheckedChangeListener(object : CompoundButton.OnCheckedChangeListener {
-                        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-                            val d = items[adapterPosition]
-                            if (Shell.SU.available()) {
-                                if (Shell.Pool.SU.run("pm ${if (isChecked) "enable" else "disable"} ${constructComponentKey(d.info.packageName, d.info.name)}") == 0) {
-                                    data.info.enabled = isChecked
-                                    notifyItemChanged(adapterPosition)
-                                } else {
-                                    enabled.setOnCheckedChangeListener(null)
-                                    enabled.isChecked = !isChecked
-                                    enabled.setOnCheckedChangeListener(this)
-                                }
+                        if (Shell.SU.available()) {
+                            if (Shell.Pool.SU.run("pm ${if (isChecked) "enable" else "disable"} ${constructComponentKey(d.info.packageName, d.info.name)}") == 0) {
+                                data.info.enabled = isChecked
+                                notifyItemChanged(adapterPosition)
                             } else {
-                                Toast.makeText(context, R.string.requires_root, Toast.LENGTH_SHORT).show()
                                 enabled.setOnCheckedChangeListener(null)
                                 enabled.isChecked = !isChecked
                                 enabled.setOnCheckedChangeListener(this)
                             }
+                        } else {
+                            Toast.makeText(context, R.string.requires_root, Toast.LENGTH_SHORT).show()
+                            enabled.setOnCheckedChangeListener(null)
+                            enabled.isChecked = !isChecked
+                            enabled.setOnCheckedChangeListener(this)
                         }
-                    })
+                    }
+                })
 
-                    launch.isVisible = data.info.enabled
-                    launch.setOnClickListener {
-                        val d = items[adapterPosition]
-                        val extras = context.findExtrasForComponent(constructComponentKey(d.info.packageName, d.info.name))
+                launch.isVisible = data.info.enabled
+                launch.setOnClickListener {
+                    val d = items[adapterPosition]
+                    val extras = context.findExtrasForComponent(constructComponentKey(d.info.packageName, d.info.name))
 
-                        try {
-                            val intent = Intent(Intent.ACTION_MAIN)
-                            intent.component = ComponentName(d.info.packageName, d.info.name)
-                            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    try {
+                        val intent = Intent(Intent.ACTION_MAIN)
+                        intent.component = ComponentName(d.info.packageName, d.info.name)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+                        if (extras.isNotEmpty()) extras.forEach {
+                            intent.putExtra(it.key, it.value)
+                        }
+
+                        context.startActivity(intent)
+                    } catch (e: SecurityException) {
+                        if (Shell.SU.available()) {
+                            val command = StringBuilder("am start -n ${d.info.packageName}/${d.info.name}")
 
                             if (extras.isNotEmpty()) extras.forEach {
-                                intent.putExtra(it.key, it.value)
+                                command.append(" -e \"${it.key}\" \"${it.value}\"")
                             }
 
-                            context.startActivity(intent)
-                        } catch (e: SecurityException) {
-                            if (Shell.SU.available()) {
-                                val command = StringBuilder("am start -n ${d.info.packageName}/${d.info.name}")
-
-                                if (extras.isNotEmpty()) extras.forEach {
-                                    command.append(" -e \"${it.key}\" \"${it.value}\"")
-                                }
-
-                                Shell.Pool.SU.run(command.toString())
-                            } else {
-                                Toast.makeText(context, R.string.requires_root, Toast.LENGTH_SHORT).show()
-                            }
+                            Shell.Pool.SU.run(command.toString())
+                        } else {
+                            Toast.makeText(context, R.string.requires_root, Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
