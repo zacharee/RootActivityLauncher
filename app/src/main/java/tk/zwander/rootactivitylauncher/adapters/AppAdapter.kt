99a4a8d3e49c63eb0d62f6f81592f1186906085d
zacharee <zachary.wander@gmail.com>
2020-04-07 23:04:46 -0400

    Use AsyncListDiffer for AppAdapter and rebind all info even with same adapter position

--- app/src/main/java/tk/zwander/rootactivitylauncher/adapters/AppAdapter.kt
+++ app/src/main/java/tk/zwander/rootactivitylauncher/adapters/AppAdapter.kt
@@ -6,8 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.SortedList
 import com.simplecityapps.recyclerview_fastscroll.views.FastScrollRecyclerView
 import kotlinx.android.synthetic.main.app_item.view.*
 import tk.zwander.rootactivitylauncher.R
@@ -18,38 +19,21 @@ import tk.zwander.rootactivitylauncher.picasso.AppIconHandler
 import tk.zwander.rootactivitylauncher.util.InnerDividerItemDecoration
 import tk.zwander.rootactivitylauncher.util.forEachParallel
 import tk.zwander.rootactivitylauncher.util.picasso
+import kotlin.Comparator
 import kotlin.collections.HashMap
 
 class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
     FastScrollRecyclerView.SectionedAdapter {
-    val items = SortedList(AppInfo::class.java, object : SortedList.Callback<AppInfo>() {
-        override fun areItemsTheSame(item1: AppInfo, item2: AppInfo) =
-            item1.info.packageName == item2.info.packageName
-
-        override fun onMoved(fromPosition: Int, toPosition: Int) {
-            notifyItemMoved(fromPosition, toPosition)
-        }
-
-        override fun onChanged(position: Int, count: Int) {
-            notifyItemRangeChanged(position, count)
-        }
-
-        override fun onInserted(position: Int, count: Int) {
-            notifyItemRangeInserted(position, count)
-        }
-
-        override fun onRemoved(position: Int, count: Int) {
-            notifyItemRangeRemoved(position, count)
-        }
-
-        override fun compare(o1: AppInfo, o2: AppInfo) =
-            o1.label.toString().compareTo(o2.label.toString(), ignoreCase = true)
-
+    val async = AsyncListDiffer(this, object : DiffUtil.ItemCallback<AppInfo>() {
         override fun areContentsTheSame(oldItem: AppInfo, newItem: AppInfo): Boolean {
             return false
         }
 
+        override fun areItemsTheSame(oldItem: AppInfo, newItem: AppInfo): Boolean {
+            return oldItem.info.packageName == newItem.info.packageName
+        }
     })
+
     private val orig = HashMap<String, AppInfo>()
     private val innerDividerItemDecoration =
         InnerDividerItemDecoration(context, RecyclerView.VERTICAL)
@@ -71,16 +55,8 @@ class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
     var exportedFilterMode = ExportedFilterMode.SHOW_ALL
         private set
 
-    init {
-        setHasStableIds(true)
-    }
-
-    override fun getItemId(position: Int): Long {
-        return items[position].info.packageName.hashCode().toLong()
-    }
-
     override fun getItemCount(): Int {
-        return items.size()
+        return async.currentList.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AppVH {
@@ -88,30 +64,27 @@ class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
     }
 
     override fun onBindViewHolder(holder: AppVH, position: Int) {
-        holder.bind(items[position])
+        holder.bind(async.currentList[position])
     }
 
     override fun getSectionName(position: Int): String {
-        return items[position].label.substring(0, 1)
+        return async.currentList[position].label.substring(0, 1)
     }
 
-    fun setItems(items: Collection<AppInfo>) {
+    fun setItems(items: List<AppInfo>) {
         orig.clear()
         items.forEachParallel {
             orig[it.info.packageName] = it
         }
 
         onFilterChange(override = true)
-        this.items.replaceAll(filter())
+        sortAndSubmitList(items)
     }
 
-    fun clearItems() {
-        orig.clear()
-    }
-
-    fun addItem(item: AppInfo) {
-        orig[item.info.packageName] = item
-        if (matches(item)) items.add(item)
+    private fun sortAndSubmitList(items: List<AppInfo>) {
+        async.submitList(items.sortedWith(Comparator { o1, o2 ->
+            o1.label.toString().compareTo(o2.label.toString())
+        }))
     }
 
     fun onFilterChange(
@@ -132,7 +105,7 @@ class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
                     exportedFilterMode
                 )
             }
-            items.replaceAll(filter())
+            sortAndSubmitList(filter())
         }
     }
 
@@ -159,22 +132,20 @@ class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
     }
 
     inner class AppVH(view: View) : RecyclerView.ViewHolder(view) {
-        private var prevPos = -1
-
         init {
             itemView.apply {
                 activities.addItemDecoration(innerDividerItemDecoration)
                 services.addItemDecoration(innerDividerItemDecoration)
 
                 activities_title.setOnClickListener {
-                    val d = items[adapterPosition]
+                    val d = async.currentList[adapterPosition]
                     d.activitiesExpanded = !d.activitiesExpanded
 
                     notifyItemChanged(adapterPosition)
                 }
 
                 services_title.setOnClickListener {
-                    val d = items[adapterPosition]
+                    val d = async.currentList[adapterPosition]
                     d.servicesExpanded = !d.servicesExpanded
 
                     notifyItemChanged(adapterPosition)
@@ -184,20 +155,16 @@ class AppAdapter(context: Context) : RecyclerView.Adapter<AppAdapter.AppVH>(),
 
         fun bind(data: AppInfo) {
             itemView.apply {
-                if (prevPos != adapterPosition) {
-                    prevPos = adapterPosition
+                picasso.load(AppIconHandler.createUri(data.info.packageName))
+                    .fit()
+                    .centerInside()
+                    .into(app_icon)
 
-                    picasso.load(AppIconHandler.createUri(data.info.packageName))
-                        .fit()
-                        .centerInside()
-                        .into(app_icon)
+                app_name.text = data.label
+                app_pkg.text = data.info.packageName
 
-                    app_name.text = data.label
-                    app_pkg.text = data.info.packageName
-
-                    activities.adapter = data.activityAdapter
-                    services.adapter = data.serviceAdapter
-                }
+                activities.adapter = data.activityAdapter
+                services.adapter = data.serviceAdapter
 
                 if (activities.isVisible != data.activitiesExpanded) {
                     activities.isVisible = data.activitiesExpanded
