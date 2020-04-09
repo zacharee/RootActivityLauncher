36a479702fea44697f85c1f1a219f8703c3615d1
zacharee <zachary.wander@gmail.com>
2020-04-08 21:38:09 -0400

    Do some formatting on component info

--- app/src/main/java/tk/zwander/rootactivitylauncher/views/ComponentInfoDialog.kt
+++ app/src/main/java/tk/zwander/rootactivitylauncher/views/ComponentInfoDialog.kt
@@ -4,9 +4,9 @@ import android.content.Context
 import android.content.pm.ActivityInfo
 import android.content.pm.ComponentInfo
 import android.content.pm.ServiceInfo
-import android.util.Log
+import android.text.Html
+import android.text.Spanned
 import android.util.PrintWriterPrinter
-import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textview.MaterialTextView
@@ -19,16 +19,13 @@ class ComponentInfoDialog(context: Context, info: ComponentInfo) : MaterialAlert
         setTitle(R.string.component_info)
         setPositiveButton(android.R.string.ok, null)
 
-        val sWriter = StringWriter()
-        val pWriter = PrintWriter(sWriter)
-        val printer = PrintWriterPrinter(pWriter)
+        val message = when (info) {
+            is ActivityInfo -> processActivityInfo(info)
+            is ServiceInfo -> processServiceInfo(info)
+            else -> null
+        }
 
-        if (info is ActivityInfo) info.dump(printer, "")
-        if (info is ServiceInfo) info.dump(printer, "")
-
-        setMessage(sWriter.toString())
-        sWriter.close()
-        pWriter.close()
+        setMessage(message)
     }
 
     override fun create(): AlertDialog {
@@ -39,5 +36,68 @@ class ComponentInfoDialog(context: Context, info: ComponentInfo) : MaterialAlert
                 }
             }
         }
+    }
+
+    fun processServiceInfo(info: ServiceInfo): Spanned {
+        val sWriter = StringWriter()
+        val pWriter = PrintWriter(sWriter)
+        val printer = PrintWriterPrinter(pWriter)
+
+        info.dump(printer, "")
+
+        val string = formatDump(sWriter.toString())
+
+        sWriter.close()
+        pWriter.close()
+
+        return Html.fromHtml(
+            string
+        )
+    }
+
+    fun processActivityInfo(info: ActivityInfo): Spanned {
+        val sWriter = StringWriter()
+        val pWriter = PrintWriter(sWriter)
+        val printer = PrintWriterPrinter(pWriter)
+
+        info.dump(printer, "")
+
+        val string = formatDump(sWriter.toString())
+
+        sWriter.close()
+        pWriter.close()
+
+        return Html.fromHtml(
+            string
+        )
+    }
+
+    private fun formatDump(dump: String): String {
+        val string = StringBuilder()
+        dump.lines().apply {
+            forEachIndexed { index, it ->
+                val startsWithTwoSpaces = it.startsWith("  ")
+                val formatted = it
+                    .replace("  ", "&nbsp;&nbsp;")
+                    .replace(", ", ",&nbsp;")
+                    .replace(Regex(" "), "<br />${if (startsWithTwoSpaces) "&nbsp;&nbsp;" else ""}<b>")
+                    .replace(Regex("(\r\n|\n)"), "<br />")
+                    .replace(Regex("^&nbsp;&nbsp;"), "&nbsp;&nbsp;<b>")
+                    .replaceFirst(Regex("^(?!&nbsp;&nbsp;)"), "<b>")
+                    .replace("=", "</b>=")
+                    .replace(Regex(":$"), "</b>:")
+
+                when {
+                    index == lastIndex -> string.append(formatted)
+                    formatted.indexOf(":") == formatted.lastIndex -> string.append("$formatted<br />")
+                    else -> string.appendHtmlLn(formatted)
+                }
+            }
+        }
+        return string.toString()
+    }
+
+    private fun StringBuilder.appendHtmlLn(line: Any?): StringBuilder {
+        return append("$line<br><br>")
     }
 }
