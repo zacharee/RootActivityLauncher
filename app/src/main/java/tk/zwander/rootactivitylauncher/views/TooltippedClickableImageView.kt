9e5628b8338d13e8923ab5d60a315662431e2b1d
zacharee <zachary.wander@gmail.com>
2020-04-11 15:34:51 -0400

    Add tooltips to image buttons

---
+++ app/src/main/java/tk/zwander/rootactivitylauncher/views/TooltippedClickableImageView.kt
@@ -0,0 +1,34 @@
+package tk.zwander.rootactivitylauncher.views
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatImageView
+import com.skydoves.balloon.createBalloon
+import tk.zwander.rootactivitylauncher.R
+
+class TooltippedClickableImageView(context: Context, attrs: AttributeSet) : AppCompatImageView(context, attrs) {
+    init {
+        isClickable = true
+        isFocusable = true
+        setOnLongClickListener {
+            if (contentDescription.isNotBlank()) {
+                createBalloon(context) {
+                    arrowVisible = false
+                    text = contentDescription.toString()
+                    setPadding(8)
+                    setCornerRadius(2f)
+                    autoDismissDuration = 2500L
+                    setBackgroundColorResource(R.color.colorPrimary)
+                }.apply {
+                    showAlignTop(this@TooltippedClickableImageView)
+                    setOnBalloonOutsideTouchListener { _, _ ->
+                        dismiss()
+                    }
+                }
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
