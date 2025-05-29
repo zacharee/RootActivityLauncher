fa2c1566987701c022d2c96aeca40a9d83d6d7f4
Zachary Wander <zachary.wander@gmail.com>
2025-05-29 19:49:04 -0400

    Pull colors back into theme for pre Android 12

--- app/src/main/java/tk/zwander/rootactivitylauncher/views/theme/Theme.kt
+++ app/src/main/java/tk/zwander/rootactivitylauncher/views/theme/Theme.kt
@@ -9,6 +9,8 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
+import tk.zwander.rootactivitylauncher.R
 
 @Composable
 fun Theme(
@@ -18,11 +20,23 @@ fun Theme(
     val context = LocalContext.current
     val isAndroid12 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
 
+    val colorPrimary = colorResource(R.color.colorPrimary)
+    val colorPrimaryDark = colorResource(R.color.colorPrimaryDark)
+    val colorAccent = colorResource(R.color.colorAccent)
+
     MaterialTheme(
         colorScheme = if (dark) {
-            if (isAndroid12) dynamicDarkColorScheme(context) else darkColorScheme()
+            if (isAndroid12) dynamicDarkColorScheme(context) else darkColorScheme(
+                primary = colorPrimary,
+                secondary = colorAccent,
+                tertiary = colorPrimaryDark,
+            )
         } else {
-            if (isAndroid12) dynamicLightColorScheme(context) else lightColorScheme()
+            if (isAndroid12) dynamicLightColorScheme(context) else lightColorScheme(
+                primary = colorPrimary,
+                secondary = colorAccent,
+                tertiary = colorPrimaryDark,
+            )
         },
         content = content,
     )
